da77b17d3e633699254669bb2d0d2ef88f3cdda7
3RaghavK3 <33raghavk33@gmail.com>
2026-02-25 23:46:26 +0530

    setting up eslint rules for the frontend

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,12 +7,20 @@ const eslintConfig = defineConfig([
   ...nextTs,
   // Override default ignores of eslint-config-next.
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "react-hooks/exhaustive-deps": "warn",
+      "@typescript-eslint/no-require-imports": "warn",
+      "react-hooks/rules-of-hooks": "warn"
+    }
+  }
 ]);
 
 export default eslintConfig;
